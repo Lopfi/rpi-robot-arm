a7466ef10442054881c16f060b0039bdd5dd7078
RobinD95 <lopfi95@gmail.com>
2020-10-24 14:41:51 +0200

    sitched to adafruit-i2c-pwm-driver-async package and changed the test script

--- test.js
+++ test.js
@@ -1,53 +1,28 @@
-var i2cBus = require("i2c-bus");
-var Pca9685Driver = require("pca9685").Pca9685Driver;
+const { PwmDriver, sleep } = require('adafruit-i2c-pwm-driver-async');
  
-var options = {
-    i2c: i2cBus.openSync(1),
-    address: 0x40,
-    frequency: 50,
-    debug: false
-};
-pwm = new Pca9685Driver(options, function(err) {
-    if (err) {
-        console.error("Error initializing PCA9685");
-        process.exit(-1);
-    }
-    console.log("Initialization done");
- 
-    // Set channel 0 to turn on on step 42 and off on step 255
-    // (with optional callback)
-    pwm.setPulseRange(0, 42, 255, function() {
-        if (err) {
-            console.error("Error setting pulse range.");
-        } else {
-            console.log("Pulse range set.");
-        }
-    });
-    pwm.setPulseRange(0, 255, 42, function() {
-        if (err) {
-            console.error("Error setting pulse range.");
-        } else {
-            console.log("Pulse range set.");
-        }
-    });
-    /*
-    // Set the pulse length to 1500 microseconds for channel 2
-    pwm.setPulseLength(2, 1500);
- 
-    // Set the duty cycle to 25% for channel 8
-    pwm.setDutyCycle(8, 0.25);
- 
-    // Turn off all power to channel 6
-    // (with optional callback)
-    pwm.channelOff(6, function() {
-        if (err) {
-            console.error("Error turning off channel.");
-        } else {
-            console.log("Channel 6 is off.");
-        }
-    });
- 
-    // Turn on channel 3 (100% power)
-    pwm.channelOn(3);
-    */
+// Configure driver
+const pwm = new PwmDriver({
+  address: 0x40,
+  device: '/dev/i2c-1',
+  debug: true,
+  isMockDriver: true // Remove this if running on a Raspberry Pi
 });
+ 
+// Configure min and max servo pulse lengths
+const servoMin = 150; // Min pulse length out of 4096
+const servoMax = 600; // Max pulse length out of 4096
+ 
+const loop = () => {
+  return sleep(1)
+    .then(pwm.setPWM(0, 0, servoMin))
+    .then(sleep(1))
+    .then(pwm.setPWM(0, 0, servoMax))
+    .then(loop);
+};
+ 
+// Initialize driver and loop
+pwm.init()
+  .then(pwm.setPWMFreq(50))
+  .then(sleep(1))
+  .then(loop)
+  .catch(console.error);
