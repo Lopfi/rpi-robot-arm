349f7252f38b726eb32ca1f9cabfde6a9283bab2
RobinD95 <lopfi95@gmail.com>
2020-10-24 14:47:50 +0200

    change test script

--- test.js
+++ test.js
@@ -1,17 +1,27 @@
 const { PwmDriver, sleep } = require('adafruit-i2c-pwm-driver-async');
- 
+
 // Configure driver
 const pwm = new PwmDriver({
-  address: 0x40,
-  device: '/dev/i2c-1',
-  debug: true,
-  isMockDriver: true // Remove this if running on a Raspberry Pi
+    address: 0x40,
+    device: '/dev/i2c-1',
+    debug: true,
+    isMockDriver: true // Remove this if running on a Raspberry Pi
 });
- 
+
 // Configure min and max servo pulse lengths
 const servoMin = 150; // Min pulse length out of 4096
 const servoMax = 600; // Max pulse length out of 4096
- 
+
+pwm.init()
+pwm.setPWMFreq(50)
+sleep(1)
+
+pwm.setPWM(0, 0, servoMin)
+sleep(1)
+pwm.setPWM(0, 0, servoMax)
+
+
+/*
 const loop = () => {
   return sleep(1)
     .then(pwm.setPWM(0, 0, servoMin))
@@ -19,10 +29,10 @@ const loop = () => {
     .then(pwm.setPWM(0, 0, servoMax))
     .then(loop);
 };
- 
+
 // Initialize driver and loop
 pwm.init()
   .then(pwm.setPWMFreq(50))
   .then(sleep(1))
   .then(loop)
-  .catch(console.error);
+  .catch(console.error);*/
