85a0c4f0d6ed6304e46e7ed5539d7c58fee6820b
RobinD95 <lopfi95@gmail.com>
2020-11-02 11:00:48 +0100

    testing app.js

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-var five = require("johnny-five");
-var Raspi = require("raspi-io").RaspiIO;
-var board = new five.Board({
-  io: new Raspi()
-});
+//var five = require("johnny-five");
+//var Raspi = require("raspi-io").RaspiIO;
+//var board = new five.Board({
+//  io: new Raspi()
+//});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const controller = "PCA9685";
 
-var servos = new five.Servos([
-    {controller, pin: 0, center: true},
-    {controller, pin: 1, center: true},
-    {controller, pin: 2, center: true},
-    {controller, pin: 3, center: true},
-    {controller, pin: 4, center: true},
-    {controller, pin: 5, center: true}
-]);
+//var servos = new five.Servos([
+//    {controller, pin: 0, center: true},
+//    {controller, pin: 1, center: true},
+//    {controller, pin: 2, center: true},
+//    {controller, pin: 3, center: true},
+//    {controller, pin: 4, center: true},
+//    {controller, pin: 5, center: true}
+//]);
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, '/content', 'index.html')))
 
@@ -28,12 +28,13 @@ app.post("/setPos", (req, res) => {
     console.log(req.query);
     let pos = req.query.pos;
     let channel = req.query.pos;
-    servos[channel].to(pos);
+    //servos[channel].to(pos);
+    res.send("Position set");
 });
 
 app.get("/stop", (req, res) => {
-    servos.stop();
-    res.send("stop");
+    //servos.stop();
+    res.send("All motors stoped");
 });
 
 app.get("*", (req, res) => res.send("404"));
