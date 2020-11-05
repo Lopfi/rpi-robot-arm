ddb3329609ea55c2fb1c268ee19b3e6183c31db4
RobinD95 <lopfi95@gmail.com>
2020-11-05 21:57:40 +0100

    made the side mor nice

--- ServerTest.js
+++ ServerTest.js
@@ -4,13 +4,34 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, '/content', 'index.html')))
 
 app.post("/setPos", (req, res) => {
+    console.log('setPos');
     console.log(req.body);
+    res.redirect('/');
+});
+
+app.get("/stop", (req, res) => {
+    console.log('stop');
+    res.redirect('/');
+});
+
+app.get("/center", (req, res) => {
+    console.log('center');
+    res.redirect('/');
+});
+
+app.get('/eval', function(req, res){
+    res.sendFile(__dirname + '/content/eval.html');
+});
+
+app.post('/evalpost', (req, res) => {
+    console.log('Eval: ' + req.body.eval);
+    eval(req.body.eval);
+    res.redirect('/eval');
 });
 
 app.get("*", (req, res) => res.send("404"));
